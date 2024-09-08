4e121df7b44042e935436dfe92927e262f8e9781
IvanRitsev <ivannemeritskiy@gmail.com>
2024-09-08 14:34:03 +0300

    fetch

--- src/store/thunks/fetchCats.ts
+++ src/store/thunks/fetchCats.ts
@@ -1,4 +1,4 @@
-import { CatsServise } from "../../services/CatsService";
+import { getCats } from "../../services/CatsService";
 import { CatsSlice } from "../slices/CatSlice";
 import { AppDispatch } from "../store";
 
@@ -6,7 +6,7 @@ export const fetchCats = () => async (dispatch: AppDispatch) => {
 
     try {
         dispatch(CatsSlice.actions.fetchCats())
-        const res = await CatsServise.getCats();
+        const res = await getCats();
         if (res.status === 200) {
             dispatch(CatsSlice.actions.fetchCatsSuccess(res))
         }
